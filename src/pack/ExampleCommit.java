bbcda1fb413b7d151c2880f68df56e768e4cb8c5
yhalk <yhalk@softserveinc.com>
2020-04-18 01:30:53 +0300

    second commit

--- src/pack/ExampleCommit.java
+++ src/pack/ExampleCommit.java
@@ -1,7 +1,9 @@
 package pack;
 
 public class ExampleCommit {
-    public static void main (String[] args){
+    public static void main(String[] args) {
         System.out.println("My first commit");
+        System.out.println("and second commit");
+
     }
 }
