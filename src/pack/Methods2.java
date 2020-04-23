221236675379b9efec0e0a1b90f9bda5e2ee52bf
yhalk <yhalk@softserveinc.com>
2020-04-23 13:29:08 +0300

    Methods2

---
+++ src/pack/Methods2.java
@@ -0,0 +1,12 @@
+package pack;
+
+public class Methods2 {
+    public static void main (String[] args){
+        System.out.println(hello("Yura"));
+        System.out.println(hello("Vova"));
+
+    }
+    public static String hello(String name){
+        return "Hello my dear " + name  + " how are U?";
+    }
+}
