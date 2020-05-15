72de3254053d1ec8a17d755dcba0aa0e9a52d339
yhalk <yhalk@softserveinc.com>
2020-05-16 01:16:46 +0300

    Sum of even and odd numbers

--- src/Homework05/SumEvenAndOdd.java
+++ src/Homework05/SumEvenAndOdd.java
@@ -1,5 +1,4 @@
 package Homework05;
-
 import java.util.Scanner;
 
 public class SumEvenAndOdd {
@@ -7,38 +6,37 @@ public class SumEvenAndOdd {
     public static void main (String[] args) {
 
         int a = 0;
-        int a1 = 1;
-        int a2;
         int b = 0;
+        int c = 0;
         String input = "";
-        boolean bool = true;
+        int[] intArray = new int[8];
 
-
-        System.out.println(" Please enter positive number to see Fibonacci numbers!");
-        while (bool == true) {
+        while ( a <= 7) {
+            System.out.println(" Please enter 8 numbers and then we'll do a magic");
             try {
                 Scanner scant = new Scanner(System.in);
                 input = scant.nextLine();
-                b = Integer.parseInt(input);
-                bool = false;
+                intArray[a] = Integer.parseInt(input);
+                a++;
             } catch (NumberFormatException e) {
                 System.out.println("Please enter valid number");
             }
         }
 
-        System.out.println("So here your are first " +b+ " Fibonacci numbers)");
-        System.out.print(a+ ", " +a1);
-
-        for (int i=2; i<b; i++){
-            {
-                a2=a+a1;
-                System.out.print(", "+a2);
-                a=a1;
-                a1=a2;
+        for (int i=0; i<intArray.length; i++){
+            if (intArray[i]%2 == 0){
+                b = b + intArray[i];
             }
+            else{
+                c = c + intArray[i];
+            }
+
+        }
+        System.out.println("The sum of evev numbers is " + b);
+        System.out.println("The sum of odd numbers is " + c);
         }
 
     }
-}
+
 
 
