369ab66616bde41b4c82a3e492c4316e9d34fafe
yhalk <yhalk@softserveinc.com>
2020-07-04 23:45:49 +0300

    Test Runner Excercise1

---
+++ MavenModule/src/test/java/homework/GroupsDependencies.java
@@ -0,0 +1,37 @@
+package homework;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class GroupsDependencies {
+
+    @Test (groups = "dependancy1")
+    public void dependGroup1() {
+        System.out.println("Rule #1 - Wake up");
+    }
+    @Test (groups = "dependancy1")
+    public void dependGroup2() {
+        System.out.println("Rule #2 - Brush your teeth");
+        //  Assert.fail("OOPs failed...");
+    }
+
+    @Test (dependsOnGroups = {"dependancy1","setup/teardown"})
+    public void dependOnGroup(){
+        System.out.println("Depends on group");
+    }
+    @Test (enabled = false)
+    public void ignoredTest(){
+        System.out.println("Ignored Test");
+    }
+    @Test (groups = "setup/teardown")
+    public void setup(){
+        System.out.println("Setup");
+    }
+    @Test (groups = "setup/teardown")
+    public void teardown(){
+        System.out.println("Teardown");
+      //  Assert.fail("OOPs failed...");
+    }
+
+    }
+
--- MavenModule/src/test/java/homework/TestRunnerEx1.java
+++ MavenModule/src/test/java/homework/TestRunnerEx1.java
@@ -1,3 +1,5 @@
+package homework;
+
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
