a2adf02409d049e3b9cbb0f255dd8420ad063471
yhalk <yhalk@softserveinc.com>
2020-07-05 01:12:48 +0300

    OrderedTest

---
+++ MavenModule/src/test/java/homework/OrderedTest.java
@@ -0,0 +1,25 @@
+package homework;
+
+import org.testng.annotations.Test;
+
+public class OrderedTest {
+
+    @Test(priority = 4)
+    public void test4(){
+        System.out.println("Boom!!!");
+    }
+
+    @Test(priority = 3)
+    public void test3(){
+        System.out.println("1");
+    }
+
+    @Test(priority = 2)
+    public void test2(){
+        System.out.println("2");
+    }
+    @Test(priority = 1)
+    public void test1(){
+        System.out.println("3");
+    }
+}
