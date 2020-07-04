e2ac973cd413a8adba8e488d7e2d51351c9da8a3
yhalk <yhalk@softserveinc.com>
2020-07-05 01:06:24 +0300

    Test Runner Excercise1

--- MavenModule/src/test/java/homework/TestWithParameters.java
+++ MavenModule/src/test/java/homework/TestWithParameters.java
@@ -1,4 +1,28 @@
 package homework;
 
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
 public class TestWithParameters {
+
+    @Parameters({"param1","param2","param3"})
+    @Test
+    public void paramTest (String p1, String p2, String p3 ){
+        System.out.println("\""+ p1+ " " + p2 +p3+"\" " );
+    }
+
+    @Test(dataProvider ="getData")
+    public void testWithDateProvider(String p1, String p2, String p3 ){
+        System.out.println("Italian. Lesson1: "+ p1+p2+p3);
+    }
+    @DataProvider
+    public Object[][] getData(){
+        return new Object[][]{
+                {"Ciao! ", "Come ti ", "chiama?"},
+                {"Hi! ", "What is ", "your name"},
+                {"Ciao! ", "Mi chiamo ", "Giuseppe"},
+                {"Hi! ", "My name is ", "John Bon Jovi"}
+        };
+    }
 }
